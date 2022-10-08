aac47b3210e442e788a2ba134160bdf1ae185702
Arya Wicaksana <aryain3rd@gmail.com>
2022-10-08 19:42:28 +0700

    Refactor Bagian Dart for better DX

--- lib/screen/Meteri/Bagian.dart
+++ lib/screen/Meteri/Bagian.dart
@@ -1,8 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:v1/controller/Utama/Utama.dart';
 import 'package:v1/screen/Meteri/Utama.dart';
 
 class Bagian extends StatelessWidget {
@@ -45,30 +43,27 @@ class Bagian extends StatelessWidget {
           SizedBox(
             height: 15,
           ),
-          ...List.generate(totalBagian, (index) => index + 1)
-              .map((e) => Container(
-                    child: InkWell(
-                      onTap: () {
-                        Get.to(() => Utama(
-                              id: id,
-                              totalBagian: totalBagian,
-                              bagian: "bagian${e.toString()}",
-                            ));
-                      },
-                      child: Card(
-                          child: Container(
-                        child: Text(
-                          "Bagian ${e.toString()}",
-                          style: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.w600),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15),
+            child: Column(
+              children: List.generate(totalBagian, (index) => index + 1)
+                  .map((e) => Container(
+                        margin: EdgeInsets.only(bottom: 5),
+                        child: ListTile(
+                          onTap: () {
+                            Get.to(() => Utama(
+                                id: id,
+                                totalBagian: totalBagian,
+                                bagian: "bagian${e.toString()}"));
+                          },
+                          title: Text("Bagian ${e.toString()}"),
+                          tileColor: Colors.white,
+                          trailing: Icon(Icons.check_circle_outline_rounded),
                         ),
-                        padding: EdgeInsets.only(top: 20, bottom: 20, left: 10),
-                      )),
-                    ),
-                    width: double.infinity,
-                    padding: EdgeInsets.only(left: 15, right: 15),
-                  ))
-              .toList()
+                      ))
+                  .toList(),
+            ),
+          )
         ],
       ),
     );
